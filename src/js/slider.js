64c2e45718a371cd280d9385719691921c71d092
Sergei Maznoi <s.maznoi888@gmail.com>
2022-10-05 16:07:59 +0300

    create project

---
+++ src/js/slider.js
@@ -0,0 +1,11 @@
+import $ from "jquery";
+
+// slick slider
+
+$('.multiple-items').slick({
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  slidesToShow: 1,
+  slidesToScroll: 1
+});
